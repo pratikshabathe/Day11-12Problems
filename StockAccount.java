c4063dc7cc8701345e131d0a6a66696dc931f556
pratikshabathe <pratikshabathe11@gmail.com>
2022-11-19 15:55:12 +0530

    create new file

--- StockAccount.java
+++ StockAccount.java
@@ -1,13 +1,80 @@
 package com.blz.day11_12;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
+class Stock{
+	int buy;
+	int sell;
+}
 public class StockAccount {
-
-	private static String CompanyName;
-	private static  String stockRating;
-	private static int price;
-	private static int numOfShares;
+	
+	void stockBuySell(int p[], int size) {
+		if(size == 1) {
+			System.out.println("No profit is possible as the number of days is equal to 1");
+			return;
+		}
+		
+		int c = 0;
+		ArrayList<Stock>al = new ArrayList<Stock>();
+		int j = 0;
+		
+		while(j < size -1) {
+			while ((j < size -1) && (p[j + 1] <= p[j])) {
+				j = j+1;
+			}
+			
+			if(j == size -1) {
+				break;
+			}
+			Stock st = new Stock();	
+			st.buy = j+1;
+			j = j+1;
+			
+			while ((j < size) && (p[j] >= p[j - 1]))  
+			{  
+			j = j + 1;  
+			}  
+			   
+			st.sell = j;  
+			al.add(st);  
+			  
+			// Incrementing the number of buy or sell  
+			c = c + 1;  
+			}  
+			  
+			// displaying the solution  
+			if (c == 0)  
+			{  
+			System.out.println("Buying the stock on any given day will not make the profit.");  
+			}  
+			else  
+			{  
+			int ans = 0;  
+			for (int i = 0; i < c; i++)  
+			{  
+			System.out.println("Buy the stock on day: " + al.get(i).buy  
+			+ "  "  
+			+ "Sell the stock on day: " + al.get(i).sell);  
+			  
+			ans = ans + (p[(al.get(i).sell - 1)] - p[(al.get(i).buy - 1)]);  
+			}  
+			  
+			  
+			  
+			System.out.println("Thus, the total profit is: " + ans);  
+			  
+			}  
+			  
+			  
+			  
+			return;  
+			}  
+	
+	private static String CompanyName ;
+	private static  String stockRating ;
+	private static String price ;
+	private static String numOfShares ;
 	
 	public static String getCompanyName() {
 		return CompanyName;
@@ -16,29 +83,56 @@ public class StockAccount {
 		return stockRating;
 	}
 	
-	public static int getprice() {
+	public static String getprice() {
 		return price;
 	}
 	
-	public static int getnumOfShares() {
+	public static String getnumOfShares() {
 		return numOfShares;
 	}
 	
-	public StockAccount(String CompanyName, String stockRating, int price, int numOfShares ) {
+	public StockAccount(String CompanyName, String stockRating, String price, String numOfShares ) {
 		super();
-		this.CompanyName = " ";
-		this.stockRating = " ";
-		this.price = 20;
-		this.numOfShares = 50;
+		this.CompanyName = CompanyName;
+		this.stockRating = stockRating;
+		this.price =price ;
+		this.numOfShares = numOfShares;
 		
 	}
+	
 	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Company name :" +getCompanyName());
-		System.out.println("Stock Rating  :" +getStockRating());
-		System.out.println("Price is :" + getprice());
-		System.out.println("Number of shares  :" +getnumOfShares());
-	}
-}
+		System.out.println("Company name :" + CompanyName);
+		System.out.println("Stock Rating  :" +stockRating);
+		System.out.println("Price is :" + price);
+		System.out.println("Number of shares  :" + numOfShares);
+		
+		int price [] = { 50, 60, 70, 80, 130};
+		int size = price.length;                 //computing the size
+		System.out.println("the price of the stock on different days is: ");
+		
+		for(int i = 0; i <size; i++) {
+			System.out.println(price[i] + " ");
+		}
+		System.out.println();
+		StockAccount obj = new StockAccount(CompanyName, CompanyName, CompanyName, CompanyName);
+		int p[] = {50, 90, 130, 155, 20, 267, 347};  
+		int size1 = p.length; // computing the size  
+		  
+		System.out.println("The price of the stock on different days is: ");  
+		  
+		for(int i = 0; i < size1; i++)  
+		{  
+		// displaying the stock price  
+		System.out.print(p[i] + " ");  
+		}  
+		  
+		System.out.println("\n");  
+		  
+		// method call  
+		obj.stockBuySell(p, size1);  
+		}  
+		}  
+
